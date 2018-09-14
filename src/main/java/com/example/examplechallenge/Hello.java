a13564939010e7e5b4af903390b25a3150506f89
stenman <roger.stenman@gmail.com>
2018-09-14 23:28:42 +0200

    CRLF

--- src/main/java/com/example/examplechallenge/Hello.java
+++ src/main/java/com/example/examplechallenge/Hello.java
@@ -2,6 +2,6 @@ package com.example.examplechallenge;
 
 public class Hello {
     public static void main(String args[]) {
-        System.out.println("Hello, challenges!");
+        System.out.println("Hello, challenges!!");
     }
 }
